737b8846993d54481720906cbbe5c80ea0b2f1e9
YMQWER <162837918+YMQWER@users.noreply.github.com>
2024-12-24 20:01:36 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker.ymone.one"]: dockerHub,
+  ["quay.ymone.one"]: "https://quay.io",
+  ["gcr.ymone.one"]: "https://gcr.io",
+  ["k8s-gcr.ymone.one"]: "https://k8s.gcr.io",
+  ["k8s.ymone.one"]: "https://registry.k8s.io",
+  ["ghcr.ymone.one"]: "https://ghcr.io",
+  ["cloudsmith.ymone.one"]: "https://docker.cloudsmith.io",
+  ["ecr.ymone.one"]: "https://public.ecr.aws",
 
   // staging
   ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
